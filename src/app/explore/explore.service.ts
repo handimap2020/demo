b6cfd5c6d0fb90f25984e3f47432b3106a01ed7a
unknown <ahn3407@naver.com>
2020-11-19 02:25:18 +0900

    보다 적절한 태그 예시 추가

--- src/app/explore/explore.service.ts
+++ src/app/explore/explore.service.ts
@@ -10,7 +10,7 @@ export class ExploreService {
           x: 127.03076860251483,
           url: 'https://lh3.googleusercontent.com/proxy/-Ki0St2yEiFmkblkROCS7c5VHgrNZWBUXACWTEZHxnWfVSKKAWdIiVe8mMOmIMvgYtzKgzA_mvJz-BhsrMdrToEGcwpIaCy8ZsccUAuD4pbJQhZglQ79o7wcjEs2tVEv5crBSTPhzwBiPDf0pGaXwzkeIhe7TeWryIdFRPMCshg',
           y: 37.58610880934777,
-          tags: ['1', '2'],
+          tags: ['1', '3'],
       },
       {
           id: '1',
@@ -18,7 +18,7 @@ export class ExploreService {
           x: 127.0333557589,
           url: 'https://lh3.googleusercontent.com/proxy/ayhuBqwg7apEqItN0bZ0rlwL6p5QK05N4d77QAHqpQ9l0ApdE6AMZOu37uGvz49ns-aqunQjsQfIinV8b6BGs1mfGYyo77-4fmx0Wtm-u0hRJX_3GBStdgqPq3cqC3f5vgfZQEx731zgLISEoolFmkFl5ynYKf9Gfldl5A',
           y: 37.5888363125305,
-          tags: ['1', '2'],
+          tags: ['2'],
       },
       {
           id: '2',
@@ -26,7 +26,7 @@ export class ExploreService {
           x: 127.02101344673,
           url: 'https://yeyak.seoul.go.kr/fileDownload.web?p=/TB_PLACEATCHFL/2015/04/20/1531&n=1O6TlSV4LE2f1o479oOHa2gz9oa4y6&on=%EC%95%88%EC%95%94%EB%8F%99%20%EC%8B%A0%EC%B2%AD%EC%82%AC%20%EC%A0%84%EA%B2%BD.jpg',
           y: 37.5856063929983,
-          tags: ['1', '2'],
+          tags: ['4'],
       },
   ];
 
